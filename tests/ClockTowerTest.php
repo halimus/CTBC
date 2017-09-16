2db9f18a6dc4efa6f751c8da843b1d444e1e3c8d
workstation\Halim <halim.webdev@gmail.com>
2017-09-16 02:05:08 -0400

    Import PHPUnit core class TestCase

--- tests/ClockTowerTest.php
+++ tests/ClockTowerTest.php
@@ -6,6 +6,16 @@
  * Date: 9/16/2017
  * Time: 1:57 AM
  */
+
+namespace TDD\Test;
+//Import autolaod file in order to use PHPUnit
+require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR .'autoload.php';
+
+use PHPUnit\Framework\TestCase; //Import PHPUnit core class TestCase
+use TDD\ClockTower;
+
 class ClockTowerTest {
+
+
 
 }
