79f49013f872456b28a813e091f2ff506b0317e0
workstation\Halim <halim.webdev@gmail.com>
2017-09-16 01:50:28 -0400

    create countBells method

--- src/ClockTower.php
+++ src/ClockTower.php
@@ -30,12 +30,93 @@ class ClockTower {
             return -1;
         }
 
+        $startTime = strtotime($startTime);
+        $endTime   = strtotime($endTime);
+        $timeDiff  = ($endTime - $startTime) / 3600;
 
+        if($timeDiff > 0){ //$timeDiff positive
+            $startHour = date('H', $startTime);
+            $endHour = date('H', $endTime);
 
-        return 1;
+            $startMinute = date('i', $startTime);
+
+            if($startMinute == '00'){ // Example: $startTime = 14:00
+                $dif = ($endHour - $startHour)+1;
+                $array = $this->rangeHours($startHour, $dif);
+                $count = array_sum($array);
+                return $count;
+            }
+            else{
+                if($endHour - $startHour == 0){
+                    $count = 0;
+                    return $count;
+                }
+                else{
+                    if($startMinute != '00'){ // round the hour, Example 14:23 become 15
+                        $startHour++;
+                    }
+                    $dif = ($endHour - $startHour)+1;
+                    $array = $this->rangeHours($startHour, $dif);
+                    $count = array_sum($array);
+                    return $count;
+                }
+            }
+        }
+        else{ // $timeDiff negative
+            $startHour = date('H', $startTime);
+            $endHour = date('H', $endTime);
+
+            $startMinute = date('i', $startTime);
+            if($startMinute != '00'){
+                $startHour++;
+            }
+
+            $array1 = $this->rangeHours($startHour, 23);
+            $array2 = $this->rangeHours(0, $endHour+1);
+
+            $count = array_sum($array1) + array_sum($array2);
+            return $count;
+        }
+
+        return 0;
     }
 
+    /**
+     * @param $offset
+     * @param int $length
+     * @return array
+     */
+    private function rangeHours($offset, $length=1){
+        $hours = array(
+            0 => 12,
+            1 => 1,
+            2 => 2,
+            3 => 3,
+            4 => 4,
+            5 => 5,
+            6 => 6,
+            7 => 7,
+            8 => 8,
+            9 => 9,
+            10 => 10,
+            11 => 11,
+            12 => 12,
+            13 => 1,
+            14 => 2,
+            15 => 3,
+            16 => 4,
+            17 => 5,
+            18 => 6,
+            19 => 7,
+            20 => 8,
+            21 => 9,
+            22 => 10,
+            23 => 11
+        );
 
+        $array = array_slice($hours, $offset, $length);
+        return $array;
+    }
 
 
 }
