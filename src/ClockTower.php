beebb20654e5d0bda825205efb518dfc7a4e87fb
workstation\Halim <halim.webdev@gmail.com>
2017-09-16 01:37:47 -0400

    add gitignore, index, ClockTower files

---
+++ src/ClockTower.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Halim
+ * Date: 9/16/2017
+ * Time: 1:35 AM
+ */
+class ClockTower {
+
+    /**
+     * ClockTower constructor.
+     */
+    public function __construct() {
+
+    }
+
+    /**
+     * @param $startTime
+     * @param $endTime
+     * @return int
+     */
+    public function countBells($startTime, $endTime) {
+
+
+        return 1;
+    }
+
+
+
+
+}
