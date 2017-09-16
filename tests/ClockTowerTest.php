d8bfc70476b25fccbcfc1f03496dedcb7adfaf97
workstation\Halim <halim.webdev@gmail.com>
2017-09-16 02:08:28 -0400

    write some testCase

--- tests/ClockTowerTest.php
+++ tests/ClockTowerTest.php
@@ -14,8 +14,26 @@ require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_
 use PHPUnit\Framework\TestCase; //Import PHPUnit core class TestCase
 use TDD\ClockTower;
 
-class ClockTowerTest {
+class ClockTowerTest extends TestCase {
 
+    public function testCase1(){
+        $clockTower = new ClockTower();
+        $this->assertEquals(5, $clockTower->countBells('2:00', '3:00'), 'countBells = 5');
+    }
 
+    public function testCase2(){
+        $clockTower = new ClockTower();
+        $this->assertEquals(60, $clockTower->countBells('4:00', '11:59'), 'countBells = 60');
+    }
+
+    public function testCase3(){
+        $clockTower = new ClockTower();
+        $this->assertEquals(156, $clockTower->countBells('2:00', '1:00'), 'countBells = 156');
+    }
+
+    public function testCase4(){
+        $clockTower = new ClockTower();
+        $this->assertEquals(158, $clockTower->countBells('15:00', '15:00'), 'countBells = 158');
+    }
 
 }
