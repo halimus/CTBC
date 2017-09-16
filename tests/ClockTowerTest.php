dc2d755fc20f00923ed9eed412e9082c40bee015
workstation\Halim <halim.webdev@gmail.com>
2017-09-16 02:15:02 -0400

    add comments

--- tests/ClockTowerTest.php
+++ tests/ClockTowerTest.php
@@ -16,24 +16,36 @@ use TDD\ClockTower;
 
 class ClockTowerTest extends TestCase {
 
+    /**
+     * testCase1
+     */
     public function testCase1(){
         $clockTower = new ClockTower();
         $this->assertEquals(5, $clockTower->countBells('2:00', '3:00'), 'countBells = 5');
     }
 
+    /**
+     * testCase2
+     */
     public function testCase2(){
         $clockTower = new ClockTower();
         $this->assertEquals(60, $clockTower->countBells('4:00', '11:59'), 'countBells = 60');
     }
 
+    /**
+     * testCase3
+     */
     public function testCase3(){
         $clockTower = new ClockTower();
         $this->assertEquals(156, $clockTower->countBells('2:00', '1:00'), 'countBells = 156');
     }
 
+    /**
+     * testCase4
+     */
     public function testCase4(){
         $clockTower = new ClockTower();
-        $this->assertEquals(158, $clockTower->countBells('15:00', '15:00'), 'countBells = 158');
+        $this->assertEquals(159, $clockTower->countBells('15:00', '15:00'), 'countBells = 158');
     }
 
 }
