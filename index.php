c84108ac986d5fc10663deb68d64e3540885eccc
workstation\Halim <halim.webdev@gmail.com>
2017-09-16 01:45:55 -0400

    index file

--- index.php
+++ index.php
@@ -5,3 +5,17 @@
  * Date: 9/16/2017
  * Time: 1:34 AM
  */
+
+include 'src/ClockTower.php';
+
+echo '<br>startTime='.$startTime = '2:00'; echo '<br>endTime='.$endTime = '3:00';
+//echo '<br>startTime='.$startTime = '14:00'; echo '<br>endTime='.$endTime = '15:00';
+//echo '<br>startTime='.$startTime = '14:23'; echo '<br>endTime='.$endTime = '15:42';
+//echo '<br>startTime='.$startTime = '23:00'; echo '<br>endTime='.$endTime = '1:00';
+//echo '<br>startTime='.$startTime = '16:00'; echo '<br>endTime='.$endTime = '16:00';
+//echo '<br>startTime='.$startTime = '3:01'; echo '<br>endTime='.$endTime = '03:59';
+
+$clockTower  = new ClockTower();
+$countBells = $clockTower->countBells($startTime, $endTime);
+
+echo '<br>countBells='.$countBells;
